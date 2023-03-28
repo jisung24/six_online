92651bb95e4bb7f4fac919de74f90f0ad0704c66
신서리티코더 <85999976+jisung24@users.noreply.github.com>
2023-03-28 13:45:00 +0900

    회원가입 로직 완료

--- app.py
+++ app.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template # flask를 불러옴 
+from flask import Flask, render_template, request, redirect, url_for, flash # flask를 불러옴 
+# flask : flask연결 
+# render_template : 템플릿 엔진 페이지 띄워줌 
+# request : 데이터 처리 객체 담당! 
+
 from pymongo import MongoClient
 
 app = Flask(__name__) # flask를 실행 
 
 
-# client = MongoClient("mongodb+srv://jisung4012:wltjd4011!@jisung0.ayoemb4.mongodb.net/?retryWrites=true&w=majority")
-# db = client.test
+client = MongoClient('localhost', 27017)
+userDB = client['user'] # 회원 db (관리자 사용자 싹 다 하나로 구분없이 모을게요!)
+usercol = userDB['users'] # 회원 users column
 
-doc = {'name':'bobby','age':21}
-# db.users.insert_one(doc)
+playerDB = client['player'] # 축구선수 db
+playercol = playerDB['players'] # 축구선수 column
+
 
 
 # << GET : 메인페이지('/') >>  
@@ -20,3 +26,43 @@ def print_hello(): # callback함수
     # 아닐 시 login.html
     return render_template('index.html')
 
+
+@app.route('/register/index', methods = ['GET'])
+def player_register_page():
+    return render_template('register.html')
+
+
+# << GET : 회원가입 페이지('/sign-up/index') >> 
+@app.route('/sign-up/index', methods = ['GET'])
+def sign_up_page():
+    return render_template('signUp.html')
+
+# << POST : 회원가입 로직처리('/api/new') >> 
+@app.route('/api/new', methods = ['POST'])
+def save_user():
+    user_id = request.form['userID'] # user ID 가져옴
+    user_pw = request.form['userPW'] # user password 가져옴 
+    user_role = request.form['role'] # role 가져옴
+    user = { # user 객체 
+        'userID' : user_id,
+        'userPW' : user_pw,
+        'userRole' : user_role,
+    }
+    # 중복검사! 
+    arr = []
+    for x in usercol.find({'userID' : user_id}):
+        arr.append(x)
+
+    if len(arr) >= 1:
+        return "중복된 id가 있습니다...!"
+    else:
+        usercol.insert_one(user)
+        return redirect(url_for('login_page'))
+
+
+# 선수 등록 
+
+
+@app.route('/login/index', methods = ['GET'])
+def login_page():
+    return render_template('login.html')
---
+++ static/js/register.js
@@ -0,0 +1,5 @@
+"use strict";
+// 선수 등록 javascript
+
+// 등록 버튼을 누르면 등록 register api를 호출한다!
+const registerBtn = document.querySelector("#register");
